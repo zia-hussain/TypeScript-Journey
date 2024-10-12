9d61c9fbe8c095b2d492c85c7b67fb2a46250da4
zia-hussain <syedziashahgillani76@gmail.com>
2024-10-12 20:07:50 +0500

    Start !!

---
+++ script.ts
@@ -0,0 +1,3 @@
+// +++++++++++++++        Starting My TypeScript Journey ++++++++++++++++
+
+// Main apna TypeScript ka safar shuru kar raha hoon taake apne coding skills ko aur bhi improve karoon, aur errors se bach sakoon. TypeScript ka sabse bada faida yeh hai ke yeh code ko thoda strict banata hai, taake choti choti mistakes se bacha ja sake. Yeh meri help karega projects ko strong aur secure banane mein, kyunki ismein type-checking hoti hai jo coding ko easy aur reliable banati hai. Toh chalo, mil ke seekhte hain, dheere dheere, ek step-by-step journey main!
