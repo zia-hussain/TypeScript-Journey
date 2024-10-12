f406a1a37cc2ad8d82428bb6db2c7d2f39bb9c9a
zia-hussain <syedziashahgillani76@gmail.com>
2024-10-13 00:15:21 +0500

    setting !

--- script.ts
+++ script.ts
@@ -3,7 +3,9 @@
 // Main apna TypeScript ka safar shuru kar raha hoon taake apne coding skills ko aur bhi improve karoon, aur errors se bach sakoon. TypeScript ka sabse bada faida yeh hai ke yeh code ko thoda strict banata hai, taake choti choti mistakes se bacha ja sake. Yeh meri help karega projects ko strong aur secure banane mein, kyunki ismein type-checking hoti hai jo coding ko easy aur reliable banati hai. Toh chalo, mil ke seekhte hain, dheere dheere, ek step-by-step journey main!
 
 //********************************************
-//** TypeScript Crash Course (Basic Types)  **
+//
+// +++++++++++++++        ** TypeScript Crash Course  **      ++++++++++++++++
+
 //********************************************
 
 // 1-Types Overview:
